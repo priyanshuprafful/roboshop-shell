37ac9e2a57acd10d33cbc5e562161e67140521f3
priyanshuprafful <priyanshuprafful@gmail.com>
2023-06-16 18:21:09 +0530

    Added comments in frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -5,3 +5,5 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 systemctl enable nginx
 systemctl start nginx
+ ##Roboshop not configured or roboshop configuration is not copied as all the servers are not ready
+ ## If any command is a failure or errored , we need to stop the script there itself , we will learn that later .
