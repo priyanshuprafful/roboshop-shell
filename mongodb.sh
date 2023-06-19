7b2b5aafc00f40bad790af77f3b3436df265be17
priyanshuprafful <priyanshuprafful@gmail.com>
2023-06-20 01:15:33 +0530

    Did some changes

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,6 @@
 cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
-systemctl enable mongodb
-systemctl start mongodb
+systemctl enable mongod
+systemctl start mongod
 
+## Update of the configuration file /etc/mongod.conf from 127 .0.0.1 with 0.0.0.0
