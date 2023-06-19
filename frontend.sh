fa54c17dd88dad34b695d0b6b36b287f8a7ecfde
priyanshuprafful <priyanshuprafful@gmail.com>
2023-06-19 21:09:45 +0530

    Added Roboshop components 1. mongodb 2. catalogue Added service, repo files 1. catalogue.service 2. mongodb.repo Updated nginx-roboshop.conf file

--- frontend.sh
+++ frontend.sh
@@ -3,7 +3,9 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 systemctl enable nginx
-systemctl start nginx
+systemctl restart nginx
  ##Roboshop not configured or roboshop configuration is not copied as all the servers are not ready
  ## If any command is a failure or errored , we need to stop the script there itself , we will learn that later .
+# Now the roboshop is configured
