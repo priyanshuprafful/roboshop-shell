582c1579a9f60dab00c79c18af89a26e6a067995
priyanshuprafful <priyanshuprafful@gmail.com>
2023-06-20 02:33:18 +0530

    added color to frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,17 @@
+echo -e "\e[35mInstalling nginx\e[0m"
 yum install nginx -y
+echo -e "\e[35mRemoving Old Content\e[0m"
 rm -rf /usr/share/nginx/html/*
+echo -e "\e[35mDownloading Frontend Content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+echo -e "\e[35mExtracting Downloaded Frontend \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+echo -e "\e[35mCopying nginx configuration for roboshop \e[0m"
 cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo -e "\e[35mEnabling nginx\e[0m"
 systemctl enable nginx
+echo -e "\e[35mStarting nginx\e[0m"
 systemctl restart nginx
  ##Roboshop not configured or roboshop configuration is not copied as all the servers are not ready
  ## If any command is a failure or errored , we need to stop the script there itself , we will learn that later .
