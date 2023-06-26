c8f1d09a52a4212efdf034e48b76c1edfc92ed80
priyanshuprafful <priyanshuprafful@gmail.com>
2023-06-26 19:25:02 +0530

    updating ./roboshop-shell

--- frontend.sh
+++ frontend.sh
@@ -2,30 +2,34 @@ code_dir=$(pwd)
 log_file=/tmp/roboshop.log
 rm -f ${log_file}
 
-echo -e "\e[35mInstalling nginx\e[0m"
+print_head() {
+  echo -e "\e[35m$1\e[0m"
+}
+
+print_head "Installing nginx"
 yum install nginx -y &>>${log_file}
 
-echo -e "\e[35mRemoving Old Content\e[0m"
+print_head "Removing Old Content"
 
 rm -rf /usr/share/nginx/html/*
 
-echo -e "\e[35mDownloading Frontend Content \e[0m"
+print_head "Downloading Frontend Content"
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
 
-echo -e "\e[35mExtracting Downloaded Frontend \e[0m"
+print_head "Extracting Downloaded Frontend"
 
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
 
-echo -e "\e[35mCopying nginx configuration for roboshop \e[0m"
+print_head "Copying nginx configuration for roboshop"
 
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
-echo -e "\e[35mEnabling nginx\e[0m"
+print_head "Enabling nginx"
 
 systemctl enable nginx &>>${log_file}
 
-echo -e "\e[35mStarting nginx\e[0m"
+print_head "Starting nginx"
 systemctl restart nginx &>>${log_file}
  ##Roboshop not configured or roboshop configuration is not copied as all the servers are not ready
  ## If any command is a failure or errored , we need to stop the script there itself , we will learn that later .
