1ab1f261481dad503d7c6926dc9e9b90995b7f02
priyanshuprafful <priyanshuprafful@gmail.com>
2023-06-28 17:36:57 +0530

    updating ./roboshop-shell

--- catalogue.sh
+++ catalogue.sh
@@ -1,49 +1,49 @@
 source common.sh
 
 print_head "Configure NodeJS repo"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>{log_file}
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 
 print_head "Installing NodeJS"
-yum install nodejs -y &>>{log_file}
+yum install nodejs -y &>>${log_file}
 
 print_head "Create RoboShop User"
-useradd roboshop &>>{log_file}
+useradd roboshop &>>${log_file}
 
 print_head "Creating Application Directory"
-mkdir /app &>>{log_file}
+mkdir /app &>>${log_file}
 
 print_head "Deleting Old Content"
-rm -rf /app/* &>>{log_file}
+rm -rf /app/* &>>${log_file}
 
 print_head "Downloading App content"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>{log_file}
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
 cd /app
 
 print_head "Extracting App Content"
-unzip /tmp/catalogue.zip &>>{log_file}
+unzip /tmp/catalogue.zip &>>${log_file}
 ## cd /app ## not needed as we are already in same directory
 
 print_head "Installing NodeJS Dependencies"
-npm install &>>{log_file}
+npm install &>>${log_file}
 
 print_head "Copy SystemD service File"
-cp configs/catalogue.service /etc/systemd/system/catalogue.service &>>{log_file}
+cp configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
 
 print_head "Reload SystemD"
-systemctl daemon-reload &>>{log_file}
+systemctl daemon-reload &>>${log_file}
 
 print_head "Enable Catalogue Service"
-systemctl enable catalogue &>>{log_file}
+systemctl enable catalogue &>>${log_file}
 
 print_head "Starting Catalogue service"
-systemctl restart catalogue &>>{log_file} ## better to give restart instead of start so that any changes occur it gets impacted
+systemctl restart catalogue &>>${log_file} ## better to give restart instead of start so that any changes occur it gets impacted
 
 print_head "Copy MongoDB repo file "
-cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>{log_file}
+cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
 
 print_head "Installing Mongo Client"
-yum install mongodb-org-shell -y &>>{log_file}
+yum install mongodb-org-shell -y &>>${log_file}
 
 print_head "Load Schema "
-mongo --host mongodb.saraldevops.online </app/schema/catalogue.js &>>{log_file}
+mongo --host mongodb.saraldevops.online </app/schema/catalogue.js &>>${log_file}
 
