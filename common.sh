abbc865716bc9058f7d8f6d7b9a87025aef049a0
priyanshuprafful <priyanshuprafful@gmail.com>
2023-07-13 11:23:34 +0530

    updating ./roboshop-shell

--- common.sh
+++ common.sh
@@ -16,6 +16,24 @@ status_check() {
   fi
 }
 
+systemd_setup() {
+  print_head "Copy SystemD service File"
+    cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+    status_check $?
+
+    print_head "Reload SystemD"
+    systemctl daemon-reload &>>${log_file}
+    status_check $?
+
+    print_head "Enable ${component} Service"
+    systemctl enable ${component} &>>${log_file}
+    status_check $?
+
+    print_head "Starting ${component} service"
+    systemctl restart ${component} &>>${log_file} ## better to give restart instead of start so that any changes occur it gets impacted
+    status_check $?
+}
+
 schema_setup() {
 
     if [ "${schema_type}" == "mongo" ]; then
@@ -30,7 +48,48 @@ schema_setup() {
       print_head "Load Schema "
       mongo --host mongodb.saraldevops.online </app/schema/${component}.js &>>${log_file}
       status_check $?
+
+    elif [ "${schema_type}" =="mysql"]; then
+
+      print_head "Install MySql Client"
+      yum install mysql -y &>>${log_file}
+      status_check $?
+
+      print_head "Load Schema"
+      mysql -h mysql.saraldevops.online -uroot -p${mysql_root_password} < /app/schema/${component}.sql # loading the schema and connecting to the server
+      status_check $?
     fi
+}
+app_prereq_setup() {
+  print_head "Create RoboShop User"
+    id roboshop &>>${log_file}
+    if [ $? -ne 0 ]; then
+      useradd roboshop &>>${log_file}
+    fi
+    status_check $?
+
+
+    print_head "Creating Application Directory"
+    if [ ! -d /app ]; then
+      mkdir /app &>>${log_file}
+    fi
+    status_check $?
+
+
+    print_head "Deleting Old Content"
+    rm -rf /app/* &>>${log_file}
+    status_check $?
+
+
+    print_head "Downloading App content"
+    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+    status_check $?
+
+    cd /app
+
+    print_head "Extracting App Content"
+    unzip /tmp/${component}.zip &>>${log_file}
+    status_check $?
 }
 
 nodejs() {
@@ -46,59 +105,37 @@ nodejs() {
   status_check $?
 
 
-  print_head "Create RoboShop User"
-  id roboshop &>>${log_file}
-  if [ $? -ne 0 ]; then
-    useradd roboshop &>>${log_file}
-  fi
-  status_check $?
-
-
-  print_head "Creating Application Directory"
-  if [ ! -d /app ]; then
-    mkdir /app &>>${log_file}
-  fi
-  status_check $?
-
-
-  print_head "Deleting Old Content"
-  rm -rf /app/* &>>${log_file}
-  status_check $?
-
-
-  print_head "Downloading App content"
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
-  status_check $?
-
-  cd /app
-
-  print_head "Extracting App Content"
-  unzip /tmp/${component}.zip &>>${log_file}
-  status_check $?
+  app_prereq_setup
   ## cd /app ## not needed as we are already in same directory
 
   print_head "Installing NodeJS Dependencies"
   npm install &>>${log_file}
   status_check $?
 
-  print_head "Copy SystemD service File"
-  cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
-  status_check $?
-
-  print_head "Reload SystemD"
-  systemctl daemon-reload &>>${log_file}
-  status_check $?
-
-  print_head "Enable ${component} Service"
-  systemctl enable ${component} &>>${log_file}
-  status_check $?
-
-  print_head "Starting ${component} service"
-  systemctl restart ${component} &>>${log_file} ## better to give restart instead of start so that any changes occur it gets impacted
-  status_check $?
+  systemd_setup
 
   schema_setup
+}
 
+java() {
+  print_head "Install Maven"
+  yum install maven -y &>>${log_file}
+  status_check $?
 
+  app_prereq_setup
 
+  print_head "Downloading Dependencies & Package"
+  mvn clean package &>>${log_file}
+  mv target/${component}-1.0.jar ${component}.jar &>>${log_file}
+  status_check $?
+
+  systemd_setup # it is a function we have defined above
+
+  schema_setup # it is a function we have defined above
+
+#  systemctl daemon-reload # after loading the schema then only we are starting these services it is also fine
+ # systemctl enable shipping # after loading the schema then only we are starting these services it is also fine
+ # systemctl start shipping # after loading the schema then only we are starting these services it is also fine
+ # so here we are setting up the schema first and then only we are starting our service
+ # all these are now the part of systemd_setup
 }
---
+++ shipping.sh
@@ -0,0 +1,11 @@
+source common.sh
+
+mysql_root_password=$1
+if [ -z "${mysql_root_password}" ]; then
+  echo -e "\e[31mMissing MySql Root Password Argument\e[0m" # printing error in red color
+  exit 1
+fi
+
+component=shipping
+schema_type="mysql"
+java
