ba5fd3da950a7a0c2c56293407a3acea53c63e81
priyanshuprafful <priyanshuprafful@gmail.com>
2023-07-14 00:29:33 +0530

    updating ./roboshop-shell

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,12 +1,21 @@
 source common.sh
 
-roboshop_app_password=$1
+user_name=$1
+roboshop_app_password=$2
+
+if [-z "${user_name}" ]; then
+  echo -e "\e[31mMissing RoboShop App UserName \e[0m"
+  exit 1
+fi
+
 if [ -z "${roboshop_app_password}" ]; then
   echo -e "\e[31mMissing RoboShop App Password \e[0m"
   exit 1
 fi
 
-print_head "Setup Erland Repos"
+
+
+print_head "Setup Erlang Repos"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>${log_file}
 status_check $?
 
@@ -15,7 +24,7 @@ curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/sc
 status_check $?
 
 print_head "Installing RabbitMQ server and erlang  "
-yum install rabbitmq-server erlang -y # installing both with single yum command
+yum install rabbitmq-server erlang -y &>>${log_file} # installing both with single yum command
 status_check $?
 
 print_head "Enable RabbitMQ service"
@@ -27,7 +36,10 @@ systemctl start rabbitmq-server &>>${log_file}
 status_check $?
 
 print_head "Add Application User"
-rabbitmqctl add_user roboshop ${roboshop_app_password} &>>${log_file}
+rabbitmqctl list_users | grep ${user_name} &>>${log_file}
+if [ $? -ne 0 ]; then
+  rabbitmqctl add_user ${user_name} ${roboshop_app_password} &>>${log_file}
+fi
 status_check $?
 
 # rabbitmqctl set_user_tags roboshop adminstrator # setting tags can be ignored as well
